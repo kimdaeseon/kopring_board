65743cc89d3a612aeb208b54cd0478e27a3e7d21
moko.ko <moko.ko@kakaocorp.com>
2022-03-15 15:09:17 +0900

    test change1

--- src/main/kotlin/com/board/kopring/controller/HelloController.kt
+++ src/main/kotlin/com/board/kopring/controller/HelloController.kt
@@ -2,19 +2,50 @@ package com.board.kopring.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.reflect.KProperty
 
 @RestController
 class HelloController {
 
     @GetMapping
     fun hello(): JsonResult {
-        return JsonResult("simple Change")
+        val result = JsonResult("content", "title")
+        println(result)
+        return result
     }
 
     @GetMapping("/api")
     fun apiHello(): JsonResult {
-        return JsonResult("api hello")
+        return JsonResult("api hello", "api")
     }
 }
 
-data class JsonResult(val content: String)
+class Content(private val value: String) {
+    init {
+        println("checking")
+        checkValue(value)
+    }
+
+    private fun checkValue(value: String) {
+        if (value.length > 10) throw IllegalArgumentException("Content length is over 10")
+    }
+
+    operator fun getValue(jsonResult: JsonResult, property: KProperty<*>): String {
+        return value
+    }
+}
+
+class Title(private val value: String) {
+    operator fun getValue(jsonResult: JsonResult, property: KProperty<*>): String {
+        return value
+    }
+}
+
+data class JsonResult(
+    private val _content: String,
+    private val _title: String
+    ) {
+    val content by Content(_content)
+    val title by Title(_title)
+    val value = 1
+}
---
+++ src/test/kotlin/123.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test
+
+class `123` {
+    @Test
+    fun main() {
+        val a: String? = "aa"
+
+    }
+
+    private fun onlyString(str:String) {
+        print(str)
+    }
+}
